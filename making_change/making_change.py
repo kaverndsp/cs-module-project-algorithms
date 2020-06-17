c39fe6571f3d1ae3e2ba919d47ac770be79c9776
kaverndsp <kaverndsp@users.noreply.github.com>
2020-06-17 17:54:08 -0500

    working thru problems

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,39 @@
 
 import sys
 
-def making_change(amount, denominations):
-  # Your code here
 
-  pass
+def making_change(amount, denominations):
+    count = 0
+
+    if amount == 0:
+        return 1
+
+    if amount == 1:
+        return 1
+
+    if amount % denominations[0] == 0:
+        count += 1
+    if amount % denominations[1] == 0:
+        count += 1
+    if amount % denominations[2] == 0:
+        count += 1
+    if amount % denominations[3] == 0:
+        count += 1
+    if amount % denominations[4] == 0:
+        count += 1
+    else:
+        return count
+
+    return count
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
